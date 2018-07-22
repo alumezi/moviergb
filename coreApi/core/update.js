6560b832712f3be532307663fbe63d2048429e46
alumezi <alumezi95@gmail.com>
2018-07-22 21:36:55 +0200

    first commit

---
+++ coreApi/core/update.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+// The document client affords developers the use of native JavaScript
+// types instead of AttributeValues to simplify the JavaScript development
+// experience with Amazon DynamoDB.
+// - AWS Documentation
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.update = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+
+  // validation
+  //if (typeof data.petName !== 'string' || typeof data.petBreed !== 'string') {
+  //  console.error('Validation Failed');
+  //  callback(new Error('Couldn\'t update the todo item.'));
+  //  return;
+  //}
+  
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+    ExpressionAttributeValues: {
+      ':url': data.url,
+      ':colors': data.colors,
+      ':checked': data.checked,
+      ':updatedAt': timestamp,
+    },
+    UpdateExpression: 'SET url = :url, colors = :colors, updatedAt = :updatedAt',
+    ReturnValues: 'ALL_NEW',
+  };
+
+  // update the todo in the database
+  dynamoDb.update(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(new Error('Couldn\'t update the todo item.'));
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Attributes),
+    };
+    callback(null, response);
+  });
+};
